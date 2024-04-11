0f6e6cb4eee267479db3329e11021f7c8a1c5599
Aban Muhammed C P <abancpchengani@gmail.com>
2024-04-11 07:37:48 +0530

    added endpoints

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 	"pithub-backend/auth"
 	"pithub-backend/config"
+	"pithub-backend/repo"
 )
 
 func main() {
 	config.ConnectDb()
+
+
 	http.HandleFunc("/login", auth.Login)
-	http.HandleFunc("/signup", auth.Register)
+	http.HandleFunc("/signup", auth.Signup)
+	http.HandleFunc("/repo/new",repo.CreateRepo)
+	http.HandleFunc("/repo/checkname",repo.CheckName)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
