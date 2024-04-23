30dbcb6cb828c6455338c348ea4dad84818c9d34
Aban Muhammed C P <abancpchengani@gmail.com>
2024-04-23 15:16:02 +0530

    getrepo function starting

--- main.go
+++ main.go
@@ -11,12 +11,11 @@ import (
 func main() {
 	config.ConnectDb()
 
-
 	http.HandleFunc("/login", auth.Login)
 	http.HandleFunc("/signup", auth.Signup)
-	http.HandleFunc("/repo/new",repo.CreateRepo)
-	http.HandleFunc("/repo/checkname",repo.CheckName)
+	http.HandleFunc("/repo/new", repo.CreateRepo)
+	http.HandleFunc("/repo/checkname", repo.CheckName)
+	http.HandleFunc("/repo/", repo.GetRepo)
 
-	
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
---
+++ repo/getRepo.go
@@ -0,0 +1,21 @@
+package repo
+
+import (
+	"net/http"
+)
+
+func GetRepo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not accepted", http.StatusNotFound)
+		return
+	}
+}
