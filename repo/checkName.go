25a7f82fd0dc85aa936bb93d3375c19eb561770c
Aban Muhammed C P <abancpchengani@gmail.com>
2024-04-25 16:42:57 +0530

    allow headers to *

--- repo/checkName.go
+++ repo/checkName.go
@@ -32,7 +32,7 @@ type ReqBody struct {
 func CheckName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("CLIENT_URL"))
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
